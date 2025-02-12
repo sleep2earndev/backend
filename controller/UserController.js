04b2378b7f809765c7059ed91ef667d19d6bb5b4
Dedeo3 <yoannes.deo@ti.ukdw.ac.id>
2025-02-12 16:30:35 +0700

    Oauth feature

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,24 +1,48 @@
 const model = require("../model/UserModel");
 
-const getToken = async (req, res) => {
+const redirectURi = (req, res) => {
+  const authUrl = `https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.REDIRECT_URI)}&scope=activity%20sleep%20profile`;
+  res.redirect(authUrl);
+}
+
+const auth = async (req, res) => {
   try {
-    console.log("Headers received in request[controller]:", req.headers);
-
-    const result = await model.getToken(req.headers, req.body);
-
-    if (!result) {
-      return res.status(400).json({ message: "Invalid request or no token received" });
+    const code = req.query.code
+    if (!code) {
+      return res.status(401).json({
+        message: "authorization code is missing"
+      });
     }
 
-    res.status(200).json(result);
-  } catch (err) {
-    console.error("Error in getToken:", err); 
+    const { access_token, refresh_token } = await model.authModel(code);
+    if (!access_token || !refresh_token) {
+      throw new Error("Failed to retrieve tokens from Fitbit API");
+    }
+    console.log('token:',access_token,'refresh:', refresh_token)
+    res.cookie("access_token", access_token, { 
+      httpOnly: true,
+      secure: true, // Wajib pakai HTTPS
+      sameSite: "Lax", // Cukup untuk subdomain
+      domain: ".syaad.dev",
+      maxAge: 8 * 60 * 60 * 1000, // 8 jam
+    });
+    
+    res.cookie("refresh_token", refresh_token, { 
+      httpOnly: true,
+      secure: true,
+      sameSite: "Lax",
+      domain: ".syaad.dev",
+      maxAge: 30 * 24 * 60 * 60 * 1000, // 30 hari
+    })
+    
+    res.redirect("https://snoorefi.syaad.dev");
 
+  } catch (err) {
     res.status(err.status || 500).json({
       message: err.message || "Something went wrong",
-      error: err.error || null 
+      error: err.error || null
     });
   }
-};
+}
 
-module.exports = { getToken };
+module.exports = { auth, redirectURi };
--- model/UserModel.js
+++ model/UserModel.js
@@ -1,39 +1,25 @@
 const axios = require('axios');
 
-const getToken = async (headers, body) => {
-    const { grant_type, code, redirect_uri } = body;
-    let { authorization} = headers;
-
-    console.log("Headers Received:", headers);
-    console.log("Body Received:", body);
-
-    // validasi undefined atau kosong
-    if (!authorization) {
-        return Promise.reject({ message: "Missing Authorization header" });
-    }
-
-    return new Promise((resolve, reject) => {
-        axios.post(
+const authModel = async (code) => {
+    try {
+        const response = await axios.post(
             'https://api.fitbit.com/oauth2/token',
-            new URLSearchParams({ grant_type, code, redirect_uri }).toString(),
-            {
-                headers: {
-                    Authorization:authorization, 
-                    "Content-Type": "application/x-www-form-urlencoded"
-                }
+            new URLSearchParams({
+                grant_type: 'authorization_code',
+                code: code,
+                redirect_uri: process.env.REDIRECT_URI,
+            }), {
+            headers: {
+                Authorization: `Basic ${Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`).toString('base64')}`,
+                "Content-Type": "application/x-www-form-urlencoded"
             }
-        ).then((res) => {
-            console.log("Response from Fitbit API:", res.data);
-            resolve(res.data); 
-        }).catch((error) => {
-            console.error("Error from Fitbit API:", error.response ? error.response.data : error.message);
+        }
+        )
+        return response.data
+    } catch (error) {
+        console.error("Error from Fitbit API:", error.response ? error.response.data : error.message);
+        throw error;
+    };
+}
 
-            reject({
-                message: "Request to Fitbit API failed",
-                error: error.response ? error.response.data : error.message
-            });
-        });
-    });
-};
-
-module.exports = { getToken };
+module.exports = { authModel };
